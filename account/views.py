a4f7d0fa9d1bee1735c15735a284824247d78b67
Renato <renatosouzavv@gmail.com>
2019-09-24 17:15:29 -0300

    Adicionada funcionalidade de actions, pra exibir ações dos usuários seguidos em um feed pro usuário

--- account/views.py
+++ account/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.views.decorators.http import require_POST
 from common.decorators import ajax_required
+from actions.utils import create_action
+from actions.models import Action
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile, Contact
 
@@ -19,6 +21,7 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(request, 'account/register_done.html', 
                           {'new_user': new_user})
     else:
@@ -70,6 +73,15 @@ def user_login(request):
 
 @login_required
 def dashboard(request):
+    #Display all actions by default
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id', flat=True)
+    
+    if following_ids:
+        #If user is following others, retrieve only their actions
+        actions = actions.filter(user_id__in=following_ids)
+    
+    actions = actions[:10]
     return render(request, 'account/dashboard.html', 
                   {'section': 'dashboard'})
     
@@ -98,6 +110,7 @@ def user_follow(request):
             user = User.objects.get(id=user_id)
             if action == 'follow':
                 Contact.objects.get_or_create(user_from=request.user, user_to=user)
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(user_from=request.user, user_to=user).delete()
             return JsonResponse({'status': 'ok'})    
